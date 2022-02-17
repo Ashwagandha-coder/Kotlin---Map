e88a2f6fd9924a07c1f176390e175830592973a5
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-17 13:18:25 +0500

    map with get()

--- map/src/main.kt
+++ map/src/main.kt
@@ -3,6 +3,8 @@ fun main() {
     var x: HashMap<Int, String> = hashMapOf(1 to "Danil",2 to "Tom",3 to "Bob",4 to "Daria",5 to "Angela")
     println(x)
 
-    //
+    // get()
+
+    println(x.get(5))
 
 }
