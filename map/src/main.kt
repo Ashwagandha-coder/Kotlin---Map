40675ce8c42daaa85d9e7d4123f1ca0d71b64932
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-17 13:32:59 +0500

    map with methods: put,remove,set

--- map/src/main.kt
+++ map/src/main.kt
@@ -1,10 +1,18 @@
 fun main() {
 
-    var x: HashMap<Int, String> = hashMapOf(1 to "Danil",2 to "Tom",3 to "Bob",4 to "Daria",5 to "Angela")
+    val x: HashMap<Int, String> = hashMapOf(1 to "Danil",2 to "Tom",3 to "Bob",4 to "Daria",5 to "Angela")
     println(x)
 
     // get()
 
     println(x.get(5))
+
+    // put,set,remove
+
+    println(x.put(99, "John"))
+    println(x.set(3, "Yurii"))
+    println(x.remove(2))
+    println(x) // Output result
+
 
 }
